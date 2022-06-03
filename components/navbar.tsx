2f1f4155b2ef5a6bf4bd4f385fcdf8fdfb5065cb
Matthew <thebest20067@gmail.com>
2022-06-03 10:35:33 -0700

    Yes Auth0 Testing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,14 @@ import styles from '../styles/NavBar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import WikiDropdown from "./dropdown"
+import { useUser } from "@auth0/nextjs-auth0"
+
 
 const Navbar = () => {
+	const { user, error, isLoading } = useUser();
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>{error.message}</div>;
 	return (
 		<nav className={styles.NavBarColor}>
 			<div>
@@ -14,6 +20,8 @@ const Navbar = () => {
 				<Link href="contact"><a className={styles.NavLink}>Gallery</a></Link>
 				<Link href="donate"><a className={styles.NavLink}>Store</a></Link>
 				<Link href="https://www.forixaim.net/"><a className={styles.NavLink}>Forixaim</a></Link>
+				<Link href="/api/auth/login"><a className={styles.RightNavLink}>Sign In</a></Link>
+				<Link href="/api/auth/logout"><a className={styles.RightNavLink}>Sign Out</a></Link>
 			</div>
 		</nav>
 	)
